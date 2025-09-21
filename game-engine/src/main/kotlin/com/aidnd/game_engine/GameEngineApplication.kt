8ff3a6a55c4a4e65c7e49472d6fcff277568a873
ahmed-alzahrani <ahmedalzahrani94@gmail.com>
2025-09-20 21:21:18 -0400

    Add boiler plate for FE + Game Engine, Hello World button

---
+++ game-engine/src/main/kotlin/com/aidnd/game_engine/GameEngineApplication.kt
@@ -0,0 +1,11 @@
+package com.aidnd.game_engine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GameEngineApplication
+
+fun main(args: Array<String>) {
+	runApplication<GameEngineApplication>(*args)
+}
---
+++ game-engine/src/test/kotlin/com/aidnd/game_engine/GameEngineApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aidnd.game_engine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GameEngineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
