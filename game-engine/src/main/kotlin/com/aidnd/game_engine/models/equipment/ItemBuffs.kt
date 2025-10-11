350cc8d051d38251d56f0fc9e227a3e93c83b55a
ahmed-alzahrani <ahmedalzahrani94@gmail.com>
2025-10-11 11:49:51 -0400

    Make item buff ability scores explicit

--- game-engine/src/main/kotlin/com/aidnd/game_engine/models/equipment/ItemBuffs.kt
+++ game-engine/src/main/kotlin/com/aidnd/game_engine/models/equipment/ItemBuffs.kt
@@ -1,13 +1,13 @@
 package com.aidnd.game_engine.models.equipment
 
 data class ItemBuffs(
-    val ac: Int = 0,
-    val str: Int = 0,
-    val dex: Int = 0,
-    val con: Int = 0,
-    val int: Int = 0,
-    val wis: Int = 0,
-    val cha: Int = 0,
+    val armorClass: Int = 0,
+    val strength: Int = 0,
+    val dexterity: Int = 0,
+    val constitution: Int = 0,
+    val intelligence: Int = 0,
+    val wisdom: Int = 0,
+    val charisma: Int = 0,
     val attack: Int? = null,
     val damage: Int? = null
 )
